ba65da09a065f7f20373c267f9e3948ff01ca573
P.Narasimhulu <padmasalenarasimha@gmail.com>
2026-07-10 21:23:07 +0530

    Relational Operators

---
+++ RelationalOP.java
@@ -0,0 +1,29 @@
+public class RelationalOP {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 25;
+        //greater than
+        boolean result = x > y;
+        System.out.println("Greater than:" + result);
+
+        //Less than
+        boolean result1 = x < y;
+        System.out.println("Less than:" + result1);
+
+        //Equal to
+        boolean result2 = x == y;
+        System.out.println("Equal to:" + result2);
+
+        //Not equal to
+        boolean result3 = x != y;
+        System.out.println("Not equal to:" + result3);
+
+        //Greater than or equal to
+        boolean result4 = x >= y;
+        System.out.println("Greater than or equal to:" + result4);
+
+        //Less than or equal to
+        boolean result5 = x <= y;
+        System.out.println("Less than or equal to:" + result5);
+    }
+}
