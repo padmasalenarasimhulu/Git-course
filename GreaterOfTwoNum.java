8a8c12593998fc7c01e485f514980b1b6271007a
P.Narasimhulu <padmasalenarasimha@gmail.com>
2026-07-10 22:10:09 +0530

    Greatest number of two numbers

---
+++ GreaterOfTwoNum.java
@@ -0,0 +1,18 @@
+public class GreaterOfTwoNum {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 21;
+
+        if (num1 > num2) {
+            System.out.println(num1 + " is greater than " + num2);
+
+        }
+        else if (num2 > num1){
+            System.out.println(num2 + " is greater than " + num1);
+
+        }
+        else{
+            System.out.println("Both numbers are equal");
+        }
+    }
+}
