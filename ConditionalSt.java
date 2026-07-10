9d4797aca6618521b1fdef176646678247d12f2e
P.Narasimhulu <padmasalenarasimha@gmail.com>
2026-07-10 21:54:49 +0530

    Conditional Statements

---
+++ ConditionalSt.java
@@ -0,0 +1,14 @@
+public class ConditionalSt {
+    public static void main(String[] args) {
+        int age = 20;
+        if (age >= 18) {
+            System.out.println("You are allowed to vote.");
+
+        }
+        else {
+            System.out.println("You are not allowed to vote.");
+        }
+        
+
+    }
+}
