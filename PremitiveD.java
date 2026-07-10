39626bf4593ab26f7b13422bbff00cdaacd8af63
P.Narasimhulu <padmasalenarasimha@gmail.com>
2026-07-10 17:30:21 +0530

    Premitive Datatypes

---
+++ PremitiveD.java
@@ -0,0 +1,21 @@
+class PremitiveD  {
+    public static void main(String[] args) {
+        byte b = 126;
+        short s = 32000;
+        int i = -23;
+        long l = 12345678l;
+
+        float f = 3.14f;
+        double d = 3.14523223;
+
+        boolean bool = true;
+
+        System.out.println("Byte value: " + b );
+        System.out.println("Short value: " + s );
+        System.out.println("Int value: " + i );
+        System.out.println("Long value: " + l );
+        System.out.println("Float value: " + f );
+        System.out.println("Double value: " + d );
+        System.out.println("Boolean value: " + bool );
+    }
+}
