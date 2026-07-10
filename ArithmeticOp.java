34360c586db4f3c7e4a7341c42e05d6035070877
P.Narasimhulu <padmasalenarasimha@gmail.com>
2026-07-10 21:10:16 +0530

    Arithmetic Operators

---
+++ ArithmeticOp.java
@@ -0,0 +1,27 @@
+public class ArithmeticOp {
+    public static void main(String[] args) {
+        int a = 16;
+        int b = 5;
+        //addition
+        int sum = a + b;
+        System.out.println("Sum:" + sum);
+
+        //Subtraction
+        int diff = a - b;
+        System.out.println("Difference:" + diff);
+
+        //Multiplicatin
+        int product = a*b;
+        System.out.println("Product:" + product);
+
+        //Division
+        int Quotient = a/b;
+        System.out.println("Quotient:" + Quotient);
+
+        //Remainder
+        int remainder = a%b;
+        System.out.println("Remainder:" + remainder);
+
+
+    }
+}
