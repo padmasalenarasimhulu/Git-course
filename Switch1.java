5a00d1c671542f7620046e4bdceb6aeda5235ed6
P.Narasimhulu <padmasalenarasimha@gmail.com>
2026-07-11 10:10:40 +0530

    Switch without break

---
+++ Switch1.java
@@ -0,0 +1,24 @@
+public class Switch1 {
+    public static void main(String[] args) {
+        int n = 5;
+        switch (n) {
+            case 1 -> System.out.println("Monday");
+                
+             case 2 -> System.out.println("Tuesday");
+                
+             case 3 -> System.out.println("Wednesday");
+                
+             case 4 -> System.out.println("Thursday");
+                
+             case 5 -> System.out.println("Friday");
+                
+             case 6 -> System.out.println("Saturday");
+               
+             case 7 -> System.out.println("Sunday");
+                
+            default -> System.out.println("Enter a valid number between 1 and 7");
+
+            
+        }
+    }
+}
