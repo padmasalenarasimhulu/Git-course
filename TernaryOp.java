7cf3b55f2b8931cd61893f0960b1bbb7a25df105
P.Narasimhulu <padmasalenarasimha@gmail.com>
2026-07-11 09:57:28 +0530

    Ternary Operator (Even or Odd)

---
+++ TernaryOp.java
@@ -0,0 +1,8 @@
+public class TernaryOp {
+    public static void main(String[] args){
+        int num = 10;
+        String result = (num % 2 == 0) ? "Even" : "Odd"
+;       System.out.println("Given number " + num + " is " + result);;
+
+    }
+}
