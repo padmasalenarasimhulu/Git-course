62b0aba51a60f03c4ea1630b9c2f73a0f3487309
P.Narasimhulu <padmasalenarasimha@gmail.com>
2026-07-12 07:00:31 +0530

    While simple example

---
+++ While.java
@@ -0,0 +1,9 @@
+public class While {
+    public static void main(String[] args) {
+        int n = 1;
+        while (n <= 15){
+            System.out.println("Number is: " + n);
+            n++;
+        }
+    }
+}
