31c6aaa643dc4605e30bc8079fbe0e2faca69e12
P.Narasimhulu <padmasalenarasimha@gmail.com>
2026-07-12 07:06:50 +0530

    Do-While somple program

---
+++ DoWhile.java
@@ -0,0 +1,9 @@
+public class DoWhile {
+    public static void main(String[] args) {
+        int n = 5;
+        do{
+            System.out.println("Do-While is used to print one time even if the condition is false");
+            n++;
+        }while(n < 5);
+    }
+}
